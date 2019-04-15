a31cafb882f29c3faaafaa99f627dde5b94a7641
Eugenio <ej.muttiozavala@ugto.mx>
2019-04-15 11:15:23 +0200

    Gamma Fomula
    Try to implement a simpler way with gamma and strain rate

--- PlasticityMain.m
+++ PlasticityMain.m
@@ -10,14 +10,17 @@ delta_t=TimeTotal/istep/5;
 
 % Strain Rate
 eps_rate=[];
+eps_p=zeros(size(STRAIN));
 sigma_vec=zeros(size(STRAIN));
 for i=1:size(STRAIN)-1
     i=i+1;
     eps_n=STRAIN(i-1);
     eps_n1=STRAIN(i);
+    eps_p_n=eps_p(i-1);
+    eps_p_n1=eps_p(i);
     eps_rate(i)=(STRAIN(i)-STRAIN(i-1))/delta_t;
     
-    sigma_vec(i)=maps_plas(matprop,eps_rate,eps_n,eps_n1);
+    [eps_p(i),sigma_vec(i)]=maps_plas(matprop,eps_rate(i),eps_n,eps_n1,eps_p_n,eps_p_n1,delta_t);
 end
 
 
--- maps_plas.m
+++ maps_plas.m
@@ -1,23 +1,34 @@
-function sigma_n1 = maps_plas(matprop,eps_rate,eps_n,eps_n1)
+function [eps_p_n1,sigma_n1] = maps_plas(matprop,eps_rate,eps_n,eps_n1,eps_p_n,eps_p_n1,delta_t)
 
 E=matprop(1);
 sigma_y=matprop(2);
 
 sigma_n=E*eps_n;
-sigma_trial=E*eps_n1;
+sigma_n1=E*eps_n1;
+sigma_rate=(sigma_n1-sigma_n)/delta_t;
 
-
-
-%Perfect Plasticity
-if abs(sigma_trial)>sigma_y
-    eps_p=eps_n1-sigma_y/E;
-    sigma_trial=sigma_y*sign(sigma_trial);
-    gamma=abs(eps_rate);
+sigma_rate_trial=E*eps_rate;
+gamma=0; 
+sigma=sigma_n1;
+f=abs(sigma)-sigma_y;
+eps_p_rate=0;
+if abs(sigma_n1)>sigma_y
+    sigma=sigma_y*sign(sigma_n1);
+    if sigma_rate>0
+        gamma=eps_rate*sign(sigma_n1);
+    end
+    sigma_n1=sigma;
 else
-    gamma=0;
+    eps_p_rate=gamma*sign(sigma_n1);
+    eps_p_n1=eps_p_rate*delta_t+eps_p_n;
+    sigma_n1=E*(eps_n1+eps_p_n1);
 end
 
-sigma_n1=sigma_trial;
+
+
+
+
+
 
 
 
